87db1a9b4c259a215e7792346e192f98958b1d4d
nguyenquan28 <43335734+nguyenquan28@users.noreply.github.com>
2019-11-28 08:57:48 +0700

    push 28/11

--- js/main.js
+++ js/main.js
@@ -2,17 +2,25 @@
 // Click show from SignIn or SignUp
 document.getElementById("click-create-account").addEventListener("click", formCreate);
 document.getElementById("Click-login").addEventListener("click", formLogin );
+ 
 function formLogin(){
     document.getElementById("form-create-account").style.display = 'none'
     document.getElementById("form-login").style.display = 'block'
     document.getElementById("Click-login").style.color = 'rgb(27, 168, 255)'
     $('div.btn-signin').css('border-bottom', '3px solid rgb(27, 168, 255)');
-    document.getElementById("Click-login").style.color = 'rgb(197, 212, 221)';
-    $('div.create-account').css('border-bottom', '1px solid rgb(197, 212, 221)');
+    document.getElementById("click-create-account").style.color = 'rgb(197, 212, 221)';
+    $('div.btn-signup').css('border-bottom', '3px solid rgb(197, 212, 221)');
+    document.getElementById("title-form-login").innerHTML = 'Login'
 }
+
 function formCreate() {
     document.getElementById("form-login").style.display = 'none'
     document.getElementById("form-create-account").style.display = 'block'
-    document.getElementById("Click-login").style.color = 'rgb(27, 168, 255)'
-    $('div.btn-signin').css('border-bottom', '3px solid rgb(27, 168, 255)');    `m`
+    document.getElementById("click-create-account").style.color = 'rgb(27, 168, 255)'
+    $('div.btn-signup').css('border-bottom', '3px solid rgb(27, 168, 255)');
+    document.getElementById("Click-login").style.color = 'rgb(197, 212, 221)';
+    $('div.btn-signin').css('border-bottom', '3px solid rgb(197, 212, 221)');
+    document.getElementById("title-form-login").innerHTML = 'Create Account'
 }
+
+// 
